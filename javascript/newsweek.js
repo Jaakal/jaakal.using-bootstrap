6715290657c90660d729f98948ba12457fc3cf9f
Jaakal <jaak.kivinukk@gmail.com>
2019-12-18 01:01:51 +0200

    Finished navbar and started with main section

--- javascript/newsweek.js
+++ javascript/newsweek.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
-    $("#search").on("focus", function (){
-        $("#nav-search").toggleClass("stretch-to-full-width");
+    $("#search").on("focus", function() {
+         if ($(window).width() > 991) {
+            $("#nav-search").toggleClass("stretch-to-full-width");
+         } 
+        
         $("#search").attr("placeholder", "");
      });
 
-     $("#search").on("blur", function (){
-        $("#nav-search").toggleClass("stretch-to-full-width");
-        $("#search").attr("placeholder", "Search");
+   $("#search").on("blur", function() {
+      if ($(window).width() > 991) {
+         $("#nav-search").toggleClass("stretch-to-full-width");
+      }
+
+      $("#search").attr("placeholder", "Search");
+   });
+
+     $(".hamburger-icon").on("click", function() {
+         $(".hamburger-icon").toggleClass("rotate-full-icon");
+         $(".nav-wrapper").toggleClass("display-navbar");
+         $(".header-bottom").toggleClass("start-navbar-animation");
      });
 });
