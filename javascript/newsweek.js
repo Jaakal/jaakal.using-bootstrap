64c7359bc30520af79dca2b0a44460f98c7e0e7c
Jaakal <jaak.kivinukk@gmail.com>
2019-12-18 20:07:52 +0200

    Created featured stories, top story and tweaked a little bit the navbar animation as well

--- javascript/newsweek.js
+++ javascript/newsweek.js
@@ -1,23 +1,45 @@
+$( window ).resize(function() {
+   if ($(window).outerWidth() < 992) {
+      $("header").addClass("header-fixed");
+   } else if ($(document).scrollTop() < 95) {
+      $("header").removeClass("header-fixed");
+   }
+});
+
+$(window).scroll(function(){
+   if ($(document).scrollTop() >= 95) {
+      $("header").addClass("header-fixed");
+   } else if ($(window).outerWidth() > 991) {
+      $("header").removeClass("header-fixed");
+   }
+});
+
 $(document).ready(function() {
-    $("#search").on("focus", function() {
-         if ($(window).width() > 991) {
-            $("#nav-search").toggleClass("stretch-to-full-width");
-         } 
-        
-        $("#search").attr("placeholder", "");
-     });
+   if ($(window).outerWidth() < 992) {
+      $("header").addClass("header-fixed");
+   } else {
+      $("header").removeClass("header-fixed");
+   }
+   
+   $("#search").on("focus", function() {
+      if ($(window).outerWidth() > 991) {
+         $("#nav-search").toggleClass("stretch-to-full-width");
+      } 
+      
+      $("#search").attr("placeholder", "");
+   });
 
    $("#search").on("blur", function() {
-      if ($(window).width() > 991) {
+      if ($(window).outerWidth() > 991) {
          $("#nav-search").toggleClass("stretch-to-full-width");
       }
 
       $("#search").attr("placeholder", "Search");
    });
 
-     $(".hamburger-icon").on("click", function() {
-         $(".hamburger-icon").toggleClass("rotate-full-icon");
-         $(".nav-wrapper").toggleClass("display-navbar");
-         $(".header-bottom").toggleClass("start-navbar-animation");
-     });
+   $(".hamburger-icon").on("click", function() {
+      $(".hamburger-icon").toggleClass("rotate-full-icon");
+      $(".nav-wrapper").toggleClass("display-navmenu");
+      $(".header-bottom").toggleClass("start-navmenu-animation");
+   });
 });
